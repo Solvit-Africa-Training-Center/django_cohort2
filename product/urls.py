e62e62c72b0802bcff6c75e60f0291ccd1f2033f
FaustinNdayishimiye <faustinnet12@gmail.com>
2026-01-19 20:13:29 +0200

    Improved product and category models from ERD design

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet
+from .views import (
+    CategoryViewSet, SubCategoryViewSet, ProductViewSet, 
+    ProductImageViewSet, FeedbackViewSet
+)
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
+router.register(r'subcategories', SubCategoryViewSet)
 router.register(r'products', ProductViewSet)
+router.register(r'product-images', ProductImageViewSet)
+router.register(r'feedback', FeedbackViewSet)
 
 urlpatterns = [
           path('', include(router.urls)),
