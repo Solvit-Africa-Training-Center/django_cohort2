85c13a72ce874ab1a5aa62535689e8cc8c048aa0
USER <michelline005@gmail.com>
2026-01-20 10:42:31 +0200

    Added cart and cartItem models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from product.models import Product
 
-# Create your models here.
+import datetime
+
+class Cart(models.Model):
+   
+    session_id = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=20, default='active')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return f"Cart {self.id}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = [ 'cart','product']
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+
