6cfa8ee339154bca050bcd4c769be49d37b60fa8
Iragenashemac <iragenashemac@gmail.com>
2026-01-22 16:30:07 +0200

    removed the unneccessary media files and also done the changes in the models

--- products/models.py
+++ products/models.py
@@ -25,7 +25,6 @@ class SubCategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name='subcategories')
-
     name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
